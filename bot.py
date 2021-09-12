dd0d093d83b3be6c21cbd5f91c5148c83b92d7e0
ku4umid3e <kuchumid3e@gmail.com>
2021-09-12 13:14:06 +0300

    попытка выполнить ДЗ с планетами в TGbot

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import ephem
+import datetime
+
 
 import settings
 
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+def planet_in_constellation(update, context):
+    user_text = update.message.text.split()[-1]
+
 
 def talk_to_me(update, context):
     user_text = update.message.text
@@ -20,8 +27,10 @@ def greet_user(update, context):
 def main():
     # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
     mybot = Updater(settings.TOKEN, use_context=True)
-    
+
     dp = mybot.dispatcher
+    # модуль ephem принимает на вход название планеты на английском, например /planet Mars
+    dp.add_handler(CommandHandler("planet", planet_in_constellation))
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     # Запись в журнал о начале работы бота.
@@ -31,6 +40,6 @@ def main():
     # Запускаем бота, он будет работать, пока мы его не остановим принудительно
     mybot.idle()
 
-
+    
 if __name__ == "__main__":
     main()
