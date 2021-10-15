0c4ef88eee744c5bd37ef457794cacfd9a7414de
ku4umid3e <kuchumid3e@gmail.com>
2021-10-15 21:40:54 +0300

    Добавил опрос, эмоджи, и отправку локации

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import os
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+        ConversationHandler)
 
+from anketa import (anketa_start, anketa_name, anketa_rating,
+        anketa_comment, anketa_skip, anketa_dontknow)
 from handlers import planet_in_constellation, talk_to_me, greet_user
 
 TOKEN = os.getenv("KEY")
@@ -12,11 +15,31 @@ logging.basicConfig(filename='bot.log', level=logging.INFO)
 def main():
     # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
     mybot = Updater(TOKEN, use_context=True)
-
+    
+    anketa = ConversationHandler(
+            entry_points=[
+                MessageHandler(Filters.regex('^(Заполнить анкету)$'),
+                anketa_start)
+                ],
+            states={
+                "name": [MessageHandler(Filters.text, anketa_name)],
+                "rating":[MessageHandler(Filters.regex('^(1|2|3|4|5)$'),
+                    anketa_rating)],
+                "comment": [
+                    CommandHandler('skip', anketa_skip),
+                    MessageHandler(Filters.text, anketa_comment)
+                    ]
+            },
+            fallbacks=[MessageHandler(
+                Filters.text | Filters.video | Filters.photo | Filters.document
+                | Filters.location, anketa_dontknow
+                )]
+            )
     dp = mybot.dispatcher
     # модуль ephem принимает на вход название планеты на английском, например /planet Mars
     dp.add_handler(CommandHandler("planet", planet_in_constellation))
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(anketa)
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     # Запись в журнал о начале работы бота.
     logging.info("Бот стартовал")
--- handlers.py
+++ handlers.py
@@ -1,7 +1,12 @@
 import datetime
 import ephem
+from random import choice
+from emoji import emojize
 
 from db import db, get_or_create_user
+import settings
+from utils import main_keyboard
+
 
 def planet_in_constellation(update, context):
     
@@ -18,15 +23,35 @@ def planet_in_constellation(update, context):
 
 def talk_to_me(update, context):
     user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(user_text)
+    context.user_data['emoji'] = get_smile(context.user_data)
+    username = update.effective_user.first_name
+    update.message.reply_text(
+            f"Здастуй, {username} {get_smile(context.user_data)}! Ты написал: {user_text}"
+            )
 
 def greet_user(update, context):
     # Информационный принт в терминал
     print("Вызван /start")
+    context.user_data['emoji'] = get_smile(context.user_data)
     # Сохраним данные пользовыателя в DB
     user = get_or_create_user(db, update.effective_user, update.message.chat.id)
     # Ответное сообщение пользователю
     update.message.reply_text(
-        "Привет пользователь! Это простой бот, который пока мало что умеет."
+        f"Привет пользователь{get_smile(context.user_data)}! Это простой бот, который пока мало что умеет."
         )
+
+def get_smile(user_data):
+    # Выбор случайного эмодзи из набора.
+    if 'emiji' not in user_data:
+        smile = choice(settings.USER_EMOJI)
+        return emojize(smile, use_aliases=True)
+    return user_data['emoji']
+
+
+def usewr_coordinates(update, context):
+    context.user_data['emoji'] = get_smile(context.user_data)
+    coords = update.message.location
+    update.message.replay_text(
+            f"Ваши координаты {coords} {context.user_data['emoji']}!",
+            replay_markup=main_keyboard()
+            )
